6245cc5c8d5f4b978bf739be030848701cd9a289
Olexandr Mykhailichenko <olexandrmykhailichenko@gmail.com>
2023-05-02 20:33:05 +0200

    exesaiz van dan

--- src/index.js
+++ src/index.js
@@ -12,8 +12,12 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {
+
+function returnFirstArgument(arg) {
+  return arg;
 }
+returnFirstArgument(10);
+returnFirstArgument("привет");
 
 /*
  Задание 2:
@@ -29,8 +33,7 @@ function returnFirstArgument() {
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults(a, b) {
-}
+function sumWithDefaults(a, b) {}
 
 /*
  Задание 3:
@@ -40,8 +43,7 @@ function sumWithDefaults(a, b) {
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult(fn) {
-}
+function returnFnResult(fn) {}
 
 /*
  Задание 4:
@@ -56,8 +58,7 @@ function returnFnResult(fn) {
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter(number) {
-}
+function returnCounter(number) {}
 
 /*
  Задание 5 *:
@@ -68,8 +69,7 @@ function returnCounter(number) {
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {
-}
+function returnArgumentsArray() {}
 
 /*
  Задание 6 *:
@@ -86,14 +86,13 @@ function returnArgumentsArray() {
 
    console.log(newSum()) выведет 6
  */
-function bindFunction(fn) {
-}
+function bindFunction(fn) {}
 
 export {
-    returnFirstArgument,
-    sumWithDefaults,
-    returnArgumentsArray,
-    returnFnResult,
-    returnCounter,
-    bindFunction
-}
+  returnFirstArgument,
+  sumWithDefaults,
+  returnArgumentsArray,
+  returnFnResult,
+  returnCounter,
+  bindFunction,
+};
